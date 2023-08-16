88867274f56d7396aa4b7ae78e3514e56ef75053
CHARLES KUMAR SINGH <39948804+charlessinhh@users.noreply.github.com>
2023-08-16 20:00:51 +0530

    variable types

--- terraform_variables/main.tf
+++ terraform_variables/main.tf
@@ -9,7 +9,26 @@ resource "local_file' "devops_automate" {
     content = var.content
 }
 
+resource "local_file' "devops_automated" {
+    filename = var.content_map["file1"]
+    content = var.content_map["content2"]
+}
 
 output "devops_learning" {
     value = var.devops_learn
+}
+
+resource "local_file" "devops_map" {
+    filename = "/home/ubuntu/terraform-learning/terraform_variables/devops_map_var.txt"
+    content = var.content_map["content1"] 
+}
+
+resource "local_file" "file1" {
+    filename = var.list_file[0]
+    content = var.content_map["content1"]  
+}
+
+resource "local_file" "file2" {
+    filename = var.list_file[1]
+    content = var.content_map["content2"] 
 }
--- terraform_variables/variables.tf
+++ terraform_variables/variables.tf
@@ -9,3 +9,37 @@ variable "content" {
 
 
 variable "devops_learn" {}
+
+#variable types
+#map
+variable "content_map" {
+    type = map
+    default = {
+        "file1" = "/home/ubuntu/terraform-learning/terraform_variables/varfile.txt"
+        "content1" = "this is a cool content"
+        "content2" = "this is more cooler content"
+    }
+}
+
+#list
+variable "list_file" {
+    type = list
+    default = ["/home/ubuntu/terraform-learning/terraform_variables/file1.txt","/home/ubuntu/terraform-learning/terraform_variables/file2.txt"]
+}
+
+
+#object
+variable "aws_ec2_object" {
+    type = object({
+        name = string 
+        instances = number
+        keys = list
+        ami = string
+    })
+    default = {
+        name = "ec2_instance_test"
+        instances = 4
+        keys = ["key1.pem","key2.pem","key3.pem","key4.pem"]
+        ami = "ubuntu"
+    }
+}
